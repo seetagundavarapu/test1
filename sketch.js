d9ca9aa371d78295e348fd3d206a2fc7f2d48adf
seetagundavarapu <87170410+seetagundavarapu@users.noreply.github.com>
2021-08-20 16:19:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect=createSprite(400, 200, 50, 80);
+  fixedRect.shapeColor="green";
+  fixedRect.debug="true";
+  movingRect=createSprite(600,200,80,30);
+  movingRect.shapeColor="green";
+  movingRect.debug="true";
+ }
+
+function draw() {
+  background(20,255,255);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  console.log(movingRect.x - fixedRect.x);
+ if( isTouching()){
+  fixedRect.shapeColor="red";
+  movingRect.shapeColor="red";
+ }
+ else{
+  fixedRect.shapeColor="green";
+    movingRect.shapeColor="green";
+ }
+ 
+    drawSprites();
+}
+
+function isTouching(){
+
+  if((fixedRect.x -movingRect.x   < fixedRect.width/2 + movingRect.width/2)
+  && (movingRect.x - fixedRect.x  <  fixedRect.width/2 + movingRect.width/2) &&
+    (fixedRect.y - movingRect.y < fixedRect.height / 2 + movingRect.height / 2)
+    && (movingRect.y - fixedRect.y < fixedRect.height / 2 + movingRect.height / 2)) {
+   return true;
+    
+  }
+  else{
+
+    return false;
+  
+  }
+}
